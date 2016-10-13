5ace6aff2023125b878fc0ccc4978b32ce6116af
Bernard Potocki <bernard.potocki@imanel.org>
2016-10-13 19:22:11 +0200

    Toggling Todo reducer

--- main.js
+++ main.js
@@ -12,6 +12,16 @@ const todos = (state = [], action) => {
           completed: false
         }
       ]
+    case 'TOGGLE_TODO':
+      return state.map(todo => {
+        if (todo.id !== action.id) {
+          return todo
+        }
+
+        return Object.assign({}, todo, {
+          completed: !todo.completed
+        })
+      })
     default:
       return state
   }
@@ -22,12 +32,12 @@ const testAddTodo = () => {
   const action = {
     type: 'ADD_TODO',
     id: 0,
-    text: 'Learn Redus'
+    text: 'Learn Redux'
   }
   const stateAfter = [
     {
       id: 0,
-      text: 'Learn Redus',
+      text: 'Learn Redux',
       completed: false
     }
   ]
@@ -39,5 +49,43 @@ const testAddTodo = () => {
   ).toEqual(stateAfter)
 }
 
+const testToggleTodo = () => {
+  const stateBefore = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: false
+    }
+  ]
+  const action = {
+    type: 'TOGGLE_TODO',
+    id: 1
+  }
+  const stateAfter = [
+    {
+      id: 0,
+      text: 'Learn Redux',
+      completed: false
+    },
+    {
+      id: 1,
+      text: 'Go shopping',
+      completed: true
+    }
+  ]
+  deepFreeze(stateBefore)
+  deepFreeze(action)
+
+  expect(
+    todos(stateBefore, action)
+  ).toEqual(stateAfter)
+}
+
 testAddTodo()
+testToggleTodo()
 console.log('All tests passed.')
