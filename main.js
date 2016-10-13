3397e8317045069ad4f1bae18cef4c877a4840f6
Bernard Potocki <bernard.potocki@imanel.org>
2016-10-13 19:43:59 +0200

    Reducer Composition with Objects

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import { createStore } from 'redux'
 import expect from 'expect'
 import deepFreeze from 'deep-freeze'
 
@@ -36,65 +37,68 @@ const todos = (state = [], action) => {
   }
 }
 
-const testAddTodo = () => {
-  const stateBefore = []
-  const action = {
-    type: 'ADD_TODO',
-    id: 0,
-    text: 'Learn Redux'
+const visibilityFilter = (state = 'SHOW_ALL', action) => {
+  switch (action.type) {
+    case 'SET_VISIBILITY_FILTER':
+      return action.filter
+    default:
+      return state
   }
-  const stateAfter = [
-    {
-      id: 0,
-      text: 'Learn Redux',
-      completed: false
-    }
-  ]
-  deepFreeze(stateBefore)
-  deepFreeze(action)
-
-  expect(
-    todos(stateBefore, action)
-  ).toEqual(stateAfter)
 }
 
-const testToggleTodo = () => {
-  const stateBefore = [
-    {
-      id: 0,
-      text: 'Learn Redux',
-      completed: false
-    },
-    {
-      id: 1,
-      text: 'Go shopping',
-      completed: false
-    }
-  ]
-  const action = {
-    type: 'TOGGLE_TODO',
-    id: 1
+const todoApp = (state = {}, action) => {
+  return {
+    todos: todos(
+      state.todos,
+      action
+    ),
+    visibilityFilter: visibilityFilter(
+      state.visibilityFilter,
+      action
+    )
   }
-  const stateAfter = [
-    {
-      id: 0,
-      text: 'Learn Redux',
-      completed: false
-    },
-    {
-      id: 1,
-      text: 'Go shopping',
-      completed: true
-    }
-  ]
-  deepFreeze(stateBefore)
-  deepFreeze(action)
-
-  expect(
-    todos(stateBefore, action)
-  ).toEqual(stateAfter)
 }
 
-testAddTodo()
-testToggleTodo()
-console.log('All tests passed.')
+const store = createStore(todoApp)
+
+console.log('Initial state:')
+console.log(store.getState())
+console.log('--------------')
+
+console.log('Dispatching ADD_TODO')
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 0,
+  text: 'Learn Redux'
+})
+console.log('Current state:')
+console.log(store.getState())
+console.log('--------------')
+
+console.log('Dispatching ADD_TODO')
+store.dispatch({
+  type: 'ADD_TODO',
+  id: 1,
+  text: 'Go shopping'
+})
+console.log('Current state:')
+console.log(store.getState())
+console.log('--------------')
+
+console.log('Dispatching TOGGLE_TODO')
+store.dispatch({
+  type: 'TOGGLE_TODO',
+  id: 0
+})
+console.log('Current state:')
+console.log(store.getState())
+console.log('--------------')
+
+console.log('Dispatching SET_VISIBILITY_FILTER')
+store.dispatch({
+  type: 'SET_VISIBILITY_FILTER',
+  filter: 'SHOW_COMPLETED'
+})
+console.log('Current state:')
+console.log(store.getState())
+console.log('--------------')
