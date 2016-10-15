4b6549fd8ad88394464e4d53bf15cd3702762804
Bernard Potocki <bernard.potocki@imanel.org>
2016-10-16 01:08:45 +0200

    Schema normalizing is responsibliti of API and not actions

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,3 @@
-import { normalize } from 'normalizr'
-import * as schema from './schema'
 import * as api from '../api'
 import { getIsFetching } from '../reducers'
 
@@ -7,7 +5,7 @@ export const addTodo = (text) => (dispatch) => (
   api.addTodo(text).then(response => {
     dispatch({
       type: 'ADD_TODO_SUCCESS',
-      response: normalize(response, schema.todo),
+      response,
     })
   })
 )
@@ -27,7 +25,7 @@ export const fetchTodos = (filter) => (dispatch, getState) => {
       dispatch({
         type: 'FETCH_TODO_SUCCESS',
         filter,
-        response: normalize(response, schema.arrayOfTodos),
+        response,
       })
     },
     error => {
@@ -44,7 +42,7 @@ export const toggleTodo = (id) => (dispatch) => (
   api.toggleTodo(id).then(response => {
     dispatch({
       type: 'TOGGLE_TODO_SUCCESS',
-      response: normalize(response, schema.todo),
+      response,
     })
   })
 )
--- src/api/index.js
+++ src/api/index.js
@@ -1,3 +1,6 @@
+import { normalize } from 'normalizr'
+import * as schema from './schema'
+
 const fakeDatabase = {
   todos: [{
     id: Math.random(),
@@ -21,11 +24,20 @@ export const fetchTodos = (filter) => (
   delay(500).then(() => {
     switch (filter) {
       case 'all':
-        return fakeDatabase.todos
+        return normalize(
+          fakeDatabase.todos,
+          schema.arrayOfTodos
+        )
       case 'active':
-        return fakeDatabase.todos.filter(t => !t.completed)
+        return normalize(
+          fakeDatabase.todos.filter(t => !t.completed),
+          schema.arrayOfTodos
+        )
       case 'completed':
-        return fakeDatabase.todos.filter(t => t.completed)
+        return normalize(
+          fakeDatabase.todos.filter(t => t.completed),
+          schema.arrayOfTodos
+        )
       default:
         throw new Error(`Unknown filter: ${filter}`)
     }
@@ -40,7 +52,7 @@ export const addTodo = (text) => (
       completed: false,
     }
     fakeDatabase.todos.push(todo)
-    return todo
+    return normalize(todo, schema.todo)
   })
 )
 
@@ -48,6 +60,6 @@ export const toggleTodo = (id) => (
   delay(500).then(() => {
     const todo = fakeDatabase.todos.find(t => t.id === id)
     todo.completed = !todo.completed
-    return todo
+    return normalize(todo, schema.todo)
   })
 )
